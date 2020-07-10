76b9ee812d0512428a22f874cad8ccdc05a2dbc6
o1298098 <o1298098@live.com>
2020-07-10 09:45:24 +0800

    Remove ignored files

--- ios/Pods/Target Support Files/permission_handler/permission_handler-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_permission_handler : NSObject
-@end
-@implementation PodsDummy_permission_handler
-@end
--- ios/Pods/Target Support Files/share_extend/share_extend-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_share_extend : NSObject
-@end
-@implementation PodsDummy_share_extend
-@end
