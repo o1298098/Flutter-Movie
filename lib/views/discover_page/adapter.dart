8a8e66f07f88cfd244996f3418f51f7643155da5
o1298098 <o1298098@live.com>
2020-08-23 09:43:41 +0800

    fixed bug

--- lib/views/discover_page/adapter.dart
+++ lib/views/discover_page/adapter.dart
@@ -1,6 +1,5 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:movie/views/discover_page/components/movicecell_component/component.dart';
-import 'reducer.dart';
 import 'state.dart';
 
 class DiscoverListAdapter extends SourceFlowAdapter<DiscoverPageState> {
@@ -9,6 +8,5 @@ class DiscoverListAdapter extends SourceFlowAdapter<DiscoverPageState> {
           pool: <String, Component<Object>>{
             'moviecell': MovieCellComponent(),
           },
-          reducer: buildReducer(),
         );
 }
